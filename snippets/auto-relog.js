ec2da6d4c7cc1ff716ec2f84cf0d684f35f76ff8
Pix3lPirat3 <83902522+Pix3lPirat3@users.noreply.github.com>
2021-05-09 02:22:16 -0700

    Create auto-relog.js

---
+++ snippets/auto-relog.js
@@ -0,0 +1,19 @@
+var options = {
+      host: 'localhost',
+      username: 'email@example.com',
+      password: '12345678'
+}
+
+var bot = mineflayer.createBot(options);
+
+function startMinecraft(bot) {
+    bot.on('kicked', function(reason) {
+        bot = mineflayer.createBot(options);
+        setTimeout(function() {
+            startMinecraft(bot);
+        }, 6000);
+    });
+    bot.on('error', console.log)
+}
+
+startMinecraft(bot);
