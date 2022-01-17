f6f6adab1e1db36214f5e5a9a2dc5608b5419e05
Pix3lPirat3 <83902522+Pix3lPirat3@users.noreply.github.com>
2022-01-16 22:12:21 -0800

    Create chat-commands.js
    An example for listening for commands in chat

---
+++ snippets/chat-commands.js
@@ -0,0 +1,18 @@
+/*
+  Usage: ".say Hello World!"
+  Usage: ".echo Hello World!"
+*/
+bot.on("chat", function (username, message) {
+  if (!message.startsWith(".")) return;
+
+  var args = message.split(" ");
+  const cmd = args[0].substring(1); // remove '.' from cmd
+  args = args.slice(1); // remove 'cmd' (args[0]) from array, only return command args
+
+  console.log("Cmd:", cmd);
+  console.log("Args:", args);
+
+  if (cmd === "say" || cmd === "echo") {
+    bot.chat("Say: " + args.join(" "));
+  }
+});
