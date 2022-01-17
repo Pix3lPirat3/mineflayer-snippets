af87ef7ae03ff60573a4e662c75f829739859061
Pix3lPirat3 <83902522+Pix3lPirat3@users.noreply.github.com>
2022-01-16 21:24:30 -0800

    Update auto-relog.js
    Changed variable names for readability, changed events, added config variable

--- snippets/auto-relog.js
+++ snippets/auto-relog.js
@@ -1,6 +1,15 @@
 const mineflayer = require('mineflayer');
 
-var minecraft;
+var config = {
+  host: 'localhost', // minecraft server ip
+  username: 'email@example.com', // minecraft username
+  password: '12345678' // minecraft password, comment out if you want to log into online-mode=false servers
+  // port: 25565,                // only set if you need a port that isn't 25565
+  // version: false,             // only set if you need a specific version or snapshot (ie: "1.8.9" or "1.16.5"), otherwise it's set automatically
+  // auth: 'mojang'              // only set if you need microsoft auth, then set this to 'microsoft'
+}
+
+var bot;
 
 /**
  * Allow the user to auto-relog after being kicked from the server.
@@ -9,24 +18,25 @@ var minecraft;
  * https://github.com/PrismarineJS/mineflayer/issues/164
  * https://github.com/PrismarineJS/mineflayer/issues/623
  */
-function startMinecraft(minecraft) {
-    minecraft = mineflayer.createBot(config.minecraft)
-    minecraft.on('login', function() {
+function startMinecraft() {
+    bot = mineflayer.createBot(config)
+    bot.on('login', function() {
         console.info("[Client] Successfully logged into account");
     });
       
-    minecraft.on('kicked', function(reason) {
-        console.log("[Kick] ", reason);
+    bot.on('kicked', function(reason) {
+        console.log("Kicked: ", reason);
+    });
+    
+    bot.on('end', function(reason) {
+        console.log("End: ", reason);
         setTimeout(function() {
-            startMinecraft(minecraft);
+            startMinecraft();
         }, 6000)
     });
     
-    /* Do not call startMinecraft on 'error', as any small error will cause a complete reboot */
-    minecraft.on('error', function(err) {
-        console.log('[Client] Error:', err)
-    })
+    bot.on('error', console.log)
     
 }
 
-startMinecraft(minecraft);
+startMinecraft();
