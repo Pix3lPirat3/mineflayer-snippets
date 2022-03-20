5ce54cc6664b6c171b7cc6ef02ef2591da509397
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-03-20 00:21:43 -0700

    Update mob-breeder.js

--- snippets/mob-breeder.js
+++ snippets/mob-breeder.js
@@ -1,37 +1,37 @@
-          bot.on('spawn', async function() {
+bot.on('spawn', async function() {
 
-            startBreeding()
+  startBreeding()
 
-            async function startBreeding() {
-              setTimeout(async function() {
+  async function startBreeding() {
+    setTimeout(async function() {
 
-                // Grab nearby sheep that are within 5 blocks, and is not a baby (!e.metadata[16] = isBaby)
-                let nearbySheep = Object.values(bot.entities).filter(e => e.name == 'sheep' && e.position.distanceTo(bot.entity.position) < 5 && !e.metadata[16]);
+      // Grab nearby sheep that are within 5 blocks, and is not a baby (!e.metadata[16] = isBaby)
+      let nearbySheep = Object.values(bot.entities).filter(e => e.name == 'sheep' && e.position.distanceTo(bot.entity.position) < 5 && !e.metadata[16]);
 
-                // Select a random sheep from the nearby sheep
-                let sheep = nearbySheep[Math.floor(Math.random() * nearbySheep.length)];
+      // Select a random sheep from the nearby sheep
+      let sheep = nearbySheep[Math.floor(Math.random() * nearbySheep.length)];
 
-                if(!sheep) {
-                  term.echo('[Warn] There are no nearby sheep to milk.');
-                  return startBreeding();
-                }
-                
-                await bot.lookAt(sheep.position, false)
+      if(!sheep) {
+        term.echo('[Warn] There are no nearby sheep to milk.');
+        return startBreeding();
+      }
 
-                let wheat = bot.inventory.items().filter(item => item.name == 'wheat')[0];
+      await bot.lookAt(sheep.position, false)
 
-                if(!wheat) {
-                  term.echo('[Warn] There is no wheat in my inventory.');
-                  return startBreeding();
-                }
+      let wheat = bot.inventory.items().filter(item => item.name == 'wheat')[0];
 
-                await bot.equip(wheat, 'hand');
-                await bot.activateEntity(sheep)
+      if(!wheat) {
+        term.echo('[Warn] There is no wheat in my inventory.');
+        return startBreeding();
+      }
 
-                startBreeding();
-              }, 2000)
-            }
+      await bot.equip(wheat, 'hand');
+      await bot.activateEntity(sheep)
+
+      startBreeding();
+    }, 2000)
+  }
 
 
 
-          });
+});
