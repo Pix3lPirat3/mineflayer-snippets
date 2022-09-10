4ef6896783638557ea5cebe6ab15bd722a8a1404
Pix3lPirat3 <pix3lpirat3@yahoo.com>
2022-09-10 14:23:28 -0700

    Update auto-clicker.js

--- snippets/auto-clicker.js
+++ snippets/auto-clicker.js
@@ -1,21 +1,40 @@
-/*
-Just say clicker start or clicker stop to.. yea..
-*/
-
-const mineflayer = require('mineflayer')
-
-// Done in ms
-const CLICK_INTERVAL = 1000
+const mineflayer = require('mineflayer');
 
 const bot = mineflayer.createBot({
-  host: '',
-  version: '',
-  username: '',
-  auth: ''
+  host: 'survival.deptofcraft.com',
+  username: 'Pix3lPirat3',
+  auth: 'microsoft'
 })
+
+let prefix = '!';
+bot.on('chat', function(username, message) {
+  const args = message.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  if(command === 'say') {
+    bot.chat(args.join(' '));
+  }
+  if(command === 'slot') {
+    if(!args.length) bot.chat('You must specify a slot (0-8)');
+    bot.setQuickBarSlot(args[0])
+    bot.chat(`Set my quick bar slot to #${args[0]}`)
+  }
+  if(command === 'clicker') {
+    if(args[0] === 'start') autoClicker.start();
+    if(args[0] === 'stop') autoClicker.stop();
+    if(args[0] === 'speed')  {
+      if(args.length < 2) bot.chat('You must specify a speed..');
+      autoClicker.click_interval = args[1];
+    }
+  }
+})
+
+bot.on('messagestr', (message) => console.log(message))
+bot.on('kicked', console.log)
+bot.on('error', console.log)
 
 const autoClicker = {
   running: undefined,
+  click_interval: 1000,
   blacklist: ['experience_orb'],
   start: () => {
     if (this.running) return
@@ -24,25 +43,9 @@ const autoClicker = {
       if (!entity) return bot.swingArm()
       if (autoClicker.blacklist.includes(entity.name)) return
       bot.attack(entity, true)
-    }, CLICK_INTERVAL)
+    }, autoClicker.click_interval)
   },
   stop: () => {
     this.running = clearInterval(this.running)
   }
 }
-
-bot.on('chat', (username, message) => {
-  if (username === bot.username) return
-
-  switch (message) {
-    case 'clicker start':
-      autoClicker.start()
-      break
-    case 'clicker stop':
-      autoClicker.stop()
-      break
-    default:
-      bot.chat('What do you want me to do? start or stop.')
-      break
-  }
-})
